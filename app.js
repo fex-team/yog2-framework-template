e884603ea7f9a0f8bdd5a02ed5981aef9b56f1a5
hefangshi <hefangshi@baidu.com>
2014-12-01 12:29:10 +0800

    change name

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+'use strict';
+
 require('yog2-kernel');
 
-var app = yog.initApp({
+var app = yog.bootstrap({
     rootPath: __dirname
+}, function(){
+    console.log('plugins load completed');
 });
 
 module.exports = app;
