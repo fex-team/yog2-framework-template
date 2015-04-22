00e5378c2750b63e887b6be410adf4457fad5cb8
hefangshi <hefangshi@gmail.com>
2015-04-22 15:10:49 +0800

    use app.js instead of bin/ww

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 'use strict';
 
-require('yog2-kernel');
+var yog = require('yog2-kernel');
 
 var app = yog.bootstrap({
     rootPath: __dirname
-}, function(){
+}, function () {
     console.log('plugins load completed');
 });
 
-module.exports = app;
+app.set('port', process.env.PORT || 8085);
+
+var server = yog.server = app.listen(app.get('port'), function () {
+    console.log('Yog server listening on port ' + server.address().port);
+});
+
+server.on('connection', function (socket) {
+    // disable nagle
+    socket.setNoDelay(true);
+    // keep-alive timeout
+    socket.setTimeout(60 * 1000);
+});
